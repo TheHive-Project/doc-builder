6676612e8e735d7756fba3b9a485c757e078fbb6
Jérôme Leonard <jerome@thehive-project.org>
2020-06-02 22:45:40 +0200

    WIP

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY ./build /
  
-COPY entrypoint.sh /usr/local/bin/
+COPY entrypoint.sh /
 
-ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
+ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,12 @@
 
 set -x
 
-GITHUB_REPO=$1
-GITHUB_ORG=$2
-GITHUB_TOKEN=$3
-
-MYPATH=`realpath $0`
-MYDIR=`dirname ${path}`
-
 clone() {
   echo "[+] Cloning repo ${GITHUB_REPO}"
   git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_ORG}/${GITHUB_REPO}.git
-}
-
-build_doc() {
-  echo "[+] Building  and publishing the doc for ${GITHUB_REPO}"
   cp build/${GITHUB_REPO}/* ${GITHUB_REPO}/
-  cd ${GITHUB_REPO}
-  /bin/bash build.sh
 }
 
 clone
-build_doc
+cd ${GITHUB_REPO}
+/bin/bash build.sh ${GITHUB_ORG} ${GITHUB_REPO} ${GITHUB_TOKEN}
